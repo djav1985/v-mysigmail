0773bd8348e8800ab34c7b411d08676fb38f7dae
Vontainment <djav1985@gmail.com>
2025-07-18 04:55:05 -0400

    feat: add templates and components

---
+++ src/components/templates/components/Tagline.vue
@@ -0,0 +1,22 @@
+<template>
+  <tr>
+    <td>
+      <span style="color:#666;font-size:12px;font-style:italic;">{{ text }}</span>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'Tagline',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
